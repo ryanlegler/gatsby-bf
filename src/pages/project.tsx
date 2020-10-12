9c2bb994ce0879e495c150fb5529f979143b4894
Ryan Legler <ryan.legler@gmail.com>
2020-10-11 20:37:52 -0700

    project page

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,42 +1,105 @@
 
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { Text, Box, jsx } from 'theme-ui';
 import React from "react";
 import { Flex } from 'jank-ui'
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Header from "../components/header";
-import ProjectThumb from "../components/projectThumb";
+import RichText from "../components/RichText";
+import Carousel from 'nuka-carousel';
+import { letterSpacing } from 'styled-system';
+import styled from "@emotion/styled";
+
+
+const StyledContainer = styled.div`
+  max-width: 1200px;
+  width: 100%;
+`
+
+
 
 const Project = (props) => {
     
-    const { categories, project = {}, logo } = props.pageContext;
-    const { images = [], name: projectName = '' } = project;
-    
+    const { categories, project = {}, logo, projects, category } = props.pageContext;
+    const { images = [], name: projectName = '', description, descriptionLink, descriptionLinkImage } = project;
+
+    const currentIndex = projects.findIndex(item => item.slug === project.slug)
+    const previousItemIndex = projects && currentIndex > 0 && currentIndex - 1;
+    const nextItemIndex = projects && currentIndex < projects.length && currentIndex + 1;
+
+    const textSx = {
+        color: "medium",
+        fontSize: "11px", 
+        fontWeight: "bold",
+        letterSpacing: "1px"
+    }
     return (
-        <div>
+        <Flex direction="vertical">
             <Header items={categories} url={logo && logo.file && logo.file.url}/>
-            <div>
-                <h1>{projectName}</h1>
-                <div>
-                    {images &&
-                        images.map((item, index) => (
+            <Flex hAlignment="center">
+                <StyledContainer>
+
+                    { images && images.length &&
+                        <Carousel wrapAround>
+                            {images.map((image, index) =>
+                                <div key={index} style={{ flex: '0 0 100%' }} >
+                                    <img style={{ objectFit: "cover", width: "100%", height: '50vh' }} src={image.file.url} />    
+                                </div>
+                            )}
+                        </Carousel>
+                    }
+                    
+                    <Box sx={{fontSize: "14px", marginTop: "50px"}} >
+                        <strong>{projectName}</strong>
+                    </Box>
+                    
+                    { description && description.json &&
+                        <Box sx={{fontSize: "14px", lineHeight: "22px", color: "medium", maxWidth: ["100%", "700px", "600px"]}}>
+                            <RichText text={description.json} />
+                        </Box>
+                    }
+
+
+                    { descriptionLink && descriptionLinkImage &&
+                        <a href={descriptionLink} target="_blank" rel="noopener noreferrer">
+                            <img style={{ objectFit: "cover", height: '50px' }} src={descriptionLinkImage.file.url} />   
+                        </a>
+                    } 
+
+                    <Flex hAlignment="right" gap="small">
+
+                        {!!nextItemIndex && projects[previousItemIndex] &&
                             <Link
-                                key={index}
-                                to="/"
-                                style={{
-                                    color: `white`,
+                                to={`/${category.slug}/${projects[previousItemIndex].slug}`}
+                                sx={{
                                     textDecoration: `none`,
+                                    ...textSx
                                 }}
                             >
-                                
-                                    <ProjectThumb url={item && item.file && item.file.url ? item.file.url : ''} title={item.name} />
-                                    
-                                
+                                previous project
                             </Link>
-                        ))}
-                </div>
-            </div>
-        </div>
+                        }
+
+                        {nextItemIndex && projects[previousItemIndex] && projects[nextItemIndex] &&
+                            <Flex shrink><span sx={textSx}>|</span></Flex>
+                        }
+
+                        {nextItemIndex && projects[nextItemIndex] &&
+                            <Link
+                                to={`/${category.slug}/${projects[nextItemIndex].slug}`}
+                                sx={{
+                                    textDecoration: `none`,
+                                    ...textSx
+                                }}
+                            >
+                                next project
+                            </Link>
+                        }
+                    </Flex>
+                
+                </StyledContainer>
+            </Flex>
+        </Flex>
     )
 
 
