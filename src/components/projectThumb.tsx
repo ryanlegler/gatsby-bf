0e2fc237419713575eed149c12abd96d5d1da0a7
Ryan Legler <ryan.legler@gmail.com>
2020-10-11 20:38:06 -0700

    project thumb

--- src/components/projectThumb.tsx
+++ src/components/projectThumb.tsx
@@ -8,26 +8,61 @@ import { Link, graphql } from "gatsby"
 import { backgroundPosition } from 'styled-system';
 
 
-const StyledThumb = styled.div<any>`
+
+const StyledImage = styled.div<any>`
   background-position: center center;
   background-size: cover;
-
-  
+  position: absolute;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
 `
 
 
+const StyledTextOverlay = styled.div<any>`
+  opacity: 0;
+  position: absolute;
+  background: rgba(0,0,0,.5);
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: opacity 300ms;
+`
 
+const StyledContainer = styled.div<any>`
+  position: relative;
+  background: rebeccapurple;
+  &:hover {
+    [data-id="overlay"] {
+      opacity: 1;
+    }
+  }
+`
 
 
 const ProjectThumb = ({ url, title }) => {
 
   return (
-    <StyledThumb sx={{
-      size: 6,
-      backgroundImage: `url(${url})`
+    <StyledContainer sx={{
+      width: '100%',
+      paddingBottom: '100%',
+      height: '0',
     }}>
-      {title}
-    </StyledThumb>
+      <StyledImage sx={{
+        backgroundImage: `url(${url})`
+      }} />
+      
+      <StyledTextOverlay data-id="overlay">
+        {title}
+      </StyledTextOverlay>
+
+    </StyledContainer>
 
   )
 }
