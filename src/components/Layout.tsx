fb5d321ee05ee311267a591d41e366a3f28850f2
Ryan Legler <ryan.legler@gmail.com>
2022-07-01 08:31:30 -0700

    minor layout fixes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,34 +3,34 @@ import { Box, jsx } from "theme-ui";
 import Header from "./header";
 
 type LayoutProps = {
-  children?: any;
-  containerSx?: any;
+    children?: any;
+    containerSx?: any;
 };
 const Layout = ({ children, containerSx = {} }: LayoutProps) => {
-  return (
-    <Box
-      sx={{
-        justifyContent: "center",
-        display: "flex",
-        width: "100%",
-        ...containerSx
-      }}
-    >
-      <Box
-        sx={{
-          maxWidth: "1600px",
-          px: [0, 3],
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          position: "relative"
-        }}
-      >
-        <Header />
-        {children && children}
-      </Box>
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                justifyContent: "center",
+                display: "flex",
+                width: "100%",
+                ...containerSx,
+            }}
+        >
+            <Box
+                sx={{
+                    maxWidth: "1100px",
+                    px: [0, 3],
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    position: "relative",
+                }}
+            >
+                <Header />
+                {children && children}
+            </Box>
+        </Box>
+    );
 };
 
 export default Layout;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,156 +16,158 @@ const CloseIcon = styled(Close)``;
 const slug = require("slug");
 
 const Header = () => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        allContentfulHomePage {
-          nodes {
-            id
-            seo {
-              id
-              title
-              description
-              keywords
-            }
-            logo {
-              file {
-                url
-              }
-            }
-            images {
-              id
-              file {
-                url
-              }
-            }
-          }
-        }
-        contentfulNavigationMenu {
-          pages {
-            ... on ContentfulPage {
-              slug
-              name
-            }
-            ... on ContentfulCategory {
-              slug
-              name
-            }
-          }
-        }
-      }
-    `
-  );
-  const { contentfulNavigationMenu, allContentfulHomePage } = data;
-
-  const { pages } = contentfulNavigationMenu ?? {};
-  const { images, logo, seo } = allContentfulHomePage.nodes[0];
-  const { url } = logo.file;
-
-  const mobileNavContext = React.useContext(MobileNavContext) || {};
-  const { open: navOpen, toggleOpen } = mobileNavContext;
-
-  const hamburgerSx: SxStyleProp = {
-    display: ["block", "none"],
-    position: "relative",
-    zIndex: [1000]
-  };
-  const navSx: SxStyleProp = {
-    display: navOpen ? ["flex", "flex", "flex"] : ["none", "none", "flex"],
-    flexDirection: ["column", "column", "row"],
-    alignItems: ["center"],
-    justifyContent: ["center"],
-    position: ["absolute", "absolute", "relative"],
-    height: ["100vh", "100vh", "initial"],
-    zIndex: [100, 100, "inital"],
-    top: [0, 0, "inital"],
-    left: [0, 0, "inital"],
-    width: ["100%", "100%", "initial"],
-    backgroundColor: ["white", "white", "transparent"],
-    "a + a": {
-      mt: [2, 2, 0],
-      ml: [0, 0, 3]
-    }
-  };
-
-  return (
-    <Box
-      className="header"
-      sx={{
-        alignItems: "center",
-        display: "flex",
-        position: "relative",
-        width: "100%",
-        maxWidth: "100%",
-        px: [21, 0, 0]
-      }}
-    >
-      <Box
-        sx={{
-          flex: "1 1 auto",
-          pt: [2, 4],
-          pb: [2, 3],
-        }}
-      >
-        <Flex vAlignment="middle" hAlignment="between">
-          <Link to="/">
-            <Box
-              sx={{
-                ...bgImageSx,
-                textIndent: "-99999px",
-                fontSize: 0.01,
-                flex: "0 0 auto",
-                height: '26px',
-                width: ['300px','410px'],
-                backgroundImage: `url(${url})`
-              }}
-            >
-              {url}
-            </Box>
-          </Link>
-
-          <Box sx={hamburgerSx} onClick={toggleOpen}>
-            {!navOpen ? <NavIcon size="30" /> : <CloseIcon size="30" />}
-          </Box>
-
-          <Box sx={navSx} onClick={toggleOpen}>
-            {pages &&
-              pages.map(page => (
-                <Link
-                  sx={{
-                    textDecoration: "none",
-                    color: "medium",
-                    "&:visited": {
-                      color: "medium"
-                    },
-                    fontSize: "12px",
-                    fontFamily: "body",
-                    fontWeight: "bold",
-                    letterSpacing: "1px",
-                    "&:hover, &.active_link": {
-                      color: "darkest"
+    const data = useStaticQuery(
+        graphql`
+            query {
+                allContentfulHomePage {
+                    nodes {
+                        id
+                        seo {
+                            id
+                            title
+                            description
+                            keywords
+                        }
+                        logo {
+                            file {
+                                url
+                            }
+                        }
+                        images {
+                            id
+                            file {
+                                url
+                            }
+                        }
                     }
-                  }}
-                  activeClassName="active_link"
-                  key={page.slug}
-                  to={`/${page.slug}`}
-                >
-                  {slug(page.name)}
-                </Link>
-              ))}
-          </Box>
-        </Flex>
-      </Box>
+                }
+                contentfulNavigationMenu {
+                    pages {
+                        ... on ContentfulPage {
+                            slug
+                            name
+                        }
+                        ... on ContentfulCategory {
+                            slug
+                            name
+                        }
+                    }
+                }
+            }
+        `
+    );
+    const { contentfulNavigationMenu, allContentfulHomePage } = data;
 
-      <Global
-        styles={{
-          body: {
-            overflow: navOpen ? "hidden" : "auto"
-          }
-        }}
-      />
-    </Box>
-  );
+    const { pages } = contentfulNavigationMenu ?? {};
+    const { images, logo, seo } = allContentfulHomePage.nodes[0];
+    const { url } = logo.file;
+
+    const mobileNavContext = React.useContext(MobileNavContext) || {};
+    const { open: navOpen, toggleOpen } = mobileNavContext;
+
+    const hamburgerSx: SxStyleProp = {
+        display: ["block", "block", "none"],
+        position: "relative",
+        zIndex: [1000],
+    };
+    const navSx: SxStyleProp = {
+        display: navOpen ? ["flex", "flex", "flex"] : ["none", "none", "flex"],
+        flexDirection: ["column", "column", "row"],
+        alignItems: ["center"],
+        justifyContent: ["center"],
+        position: ["absolute", "absolute", "relative"],
+        height: ["100vh", "100vh", "initial"],
+        zIndex: [100, 100, "initial"],
+        top: [0, 0, "initial"],
+        left: [0, 0, "initial"],
+        width: ["100%", "100%", "initial"],
+        backgroundColor: ["white", "white", "transparent"],
+        "a + a": {
+            mt: [2, 2, 0],
+            ml: [0, 0, 3],
+        },
+    };
+
+    return (
+        <Box
+            className="header"
+            sx={{
+                alignItems: "center",
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                maxWidth: "100%",
+                px: [21, 0, 0],
+            }}
+        >
+            <Box
+                sx={{
+                    flex: "1 1 auto",
+                    pt: [2, 4],
+                    pb: [2, 3],
+                }}
+            >
+                <Flex vAlignment="middle" hAlignment="between">
+                    <Link to="/">
+                        <Box
+                            sx={{
+                                ...bgImageSx,
+                                textIndent: "-99999px",
+                                fontSize: 0.01,
+                                flex: "0 0 auto",
+                                height: "21px",
+                                backgroundPosition: "left -1px",
+                                // height: "26px",
+                                width: ["260px", "410px"],
+                                backgroundImage: `url(${url})`,
+                            }}
+                        >
+                            {url}
+                        </Box>
+                    </Link>
+
+                    <Box sx={hamburgerSx} onClick={toggleOpen}>
+                        {!navOpen ? <NavIcon size="30" /> : <CloseIcon size="30" />}
+                    </Box>
+
+                    <Box sx={navSx} onClick={toggleOpen}>
+                        {pages &&
+                            pages.map((page) => (
+                                <Link
+                                    sx={{
+                                        textDecoration: "none",
+                                        color: "medium",
+                                        "&:visited": {
+                                            color: "medium",
+                                        },
+                                        fontSize: "12px",
+                                        fontFamily: "body",
+                                        fontWeight: "bold",
+                                        letterSpacing: "1px",
+                                        "&:hover, &.active_link": {
+                                            color: "darkest",
+                                        },
+                                    }}
+                                    activeClassName="active_link"
+                                    key={page.slug}
+                                    to={`/${page.slug}`}
+                                >
+                                    {slug(page.name)}
+                                </Link>
+                            ))}
+                    </Box>
+                </Flex>
+            </Box>
+
+            <Global
+                styles={{
+                    body: {
+                        overflow: navOpen ? "hidden" : "auto",
+                    },
+                }}
+            />
+        </Box>
+    );
 };
 
 export default Header;
